4e67895ddd52e5b0c530b802c365a0de8f27a0d6
zerobeloved <799818616@qq.com>
2022-02-19 14:02:42 +0800

    add Tree struct

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 [Linked List Data Structure | JavaScript](https://www.youtube.com/watch?v=ZBdE8DElQQU)
+[Trees | Data Structures in JavaScript | Make a Tree Data Structure](https://www.youtube.com/watch?v=K7VnBuOlCI8)
---
+++ linkedList.js
@@ -0,0 +1,52 @@
+const n1 = {
+    data: 100
+}
+
+const n2 = {
+    data: 200
+}
+
+n1.next = n2
+
+// console.log(n1)
+
+class Node {
+    constructor(data, next = null) {
+        this.data = data
+        this.next = next
+    }
+}
+
+const n3 = new Node(100)
+// console.log(n3)
+
+class LinkedList {
+    constructor() {
+        this.head = null
+        this.size = 0
+    }
+
+    //Insert first node
+insertFirst(data) {
+    this.head = new Node(data, this.head)
+}
+
+    //Insert last node
+
+    //Insert at index
+
+    //Get at index
+
+    //Remove at index
+
+    //Clear list
+
+    //Print list data
+}
+
+const ll = new LinkedList()
+
+ll.insertFirst(100)
+ll.insertFirst(200)
+
+console.log(ll);
